ee0d0a2042f2acbd4fab35c1df896410663f9acc
Shawnell Harrison <shawnelldh@gmail.com>
2025-10-15 20:26:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-745534590-6183c",
+  "appId": "1:592403816115:web:5970be1e0a407d8bc19ac6",
+  "apiKey": "AIzaSyA-1GRDCKGJjLgxiJZIxAM17ePm-bE_Qp8",
+  "authDomain": "studio-745534590-6183c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "592403816115"
+};
